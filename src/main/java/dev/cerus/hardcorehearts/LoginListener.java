a8982a3001e89b2e7fad18b59ae21fc77831c834
Cerus <m.dorn.2003@web.de>
2021-12-02 15:03:50 +0100

    Port new features to 1.17

--- src/main/java/dev/cerus/hardcorehearts/LoginListener.java
+++ src/main/java/dev/cerus/hardcorehearts/LoginListener.java
@@ -5,10 +5,11 @@ import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelOutboundHandlerAdapter;
 import io.netty.channel.ChannelPromise;
 import java.lang.reflect.Field;
+import net.minecraft.core.IRegistryCustom;
 import net.minecraft.network.protocol.game.PacketPlayOutLogin;
 import net.minecraft.server.level.EntityPlayer;
 import net.minecraft.server.network.ITextFilter;
-import org.bukkit.craftbukkit.v1_18_R1.entity.CraftPlayer;
+import org.bukkit.craftbukkit.v1_17_R1.entity.CraftPlayer;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerLoginEvent;
@@ -57,12 +58,12 @@ public class LoginListener implements Listener {
                     // Changing the field using reflection does not work for some reason,
                     // if you do that the client does not display any blocks
                     final PacketPlayOutLogin fakeLogin = new PacketPlayOutLogin(login.b(),
-                            true,
-                            login.d(),
                             login.e(),
                             login.f(),
+                            login.c(),
+                            true,
                             login.g(),
-                            login.h(),
+                            (IRegistryCustom.Dimension) login.h(),
                             login.i(),
                             login.j(),
                             login.k(),
@@ -70,8 +71,7 @@ public class LoginListener implements Listener {
                             login.m(),
                             login.n(),
                             login.o(),
-                            login.p(),
-                            login.q());
+                            login.p());
                     super.write(ctx, fakeLogin, promise);
                 } else {
                     super.write(ctx, msg, promise);
